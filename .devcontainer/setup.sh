233c36c9b24b17406baa48dbfdb582dcd7174009
César Fernando <cesar.fernando@proemail.mx>
2026-06-02 22:50:29 -0600

    feat(init): first commit

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# sudo chown -R ${UID}:${UID} ${HOME}/.codex
+
+# Fix project file permissions to standard privileges
+sudo chown -R ${UID}:${UID} ${PWD}
+sudo find ${PWD} -type d -exec chmod 755 {} +
+sudo find ${PWD} -type f ! -name "*.sh" -exec chmod 644 {} +
+sudo find ${PWD} -type f -name "*.sh" -exec chmod 755 {} +
+
+# Setup git files configurations
+sudo chown -R ${UID}:${UID} ${HOME}/.gitconfig-volume
+
+touch ${HOME}/.gitconfig-volume/config
+ln -fs ${HOME}/.gitconfig-volume/config ${HOME}/.gitconfig
+sudo chown -R ${UID}:${UID} ${HOME}/.gitconfig
+
+touch ${HOME}/.gitconfig-volume/.git-credentials
+ln -fs ${HOME}/.gitconfig-volume/.git-credentials ${HOME}/.git-credentials
+sudo chown -R ${UID}:${UID} ${HOME}/.git-credentials
+
+git config --global --add safe.directory ${PWD}
